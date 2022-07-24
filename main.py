c50b8ec9fad61a107522a38329cd27797ea96227
G8torx <104470093+G8torx@users.noreply.github.com>
2022-07-24 10:47:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -19,25 +19,28 @@ async def problem(ctx):
     question = ctx.message.content[len('$problem'):].strip()
     mode = ctx.message.content.split()[-1]
     question = question.strip(mode)
+    dm = await ctx.author.create_dm()
 
     if mode in ctx.message.content:
         if mode == "easy":
             with open('questions/easy.txt', 'a') as f:
                 f.write(question + '\n\n')
-            await ctx.channel.send(f"New {mode} question added!")
+            await ctx.send(f"New {mode} question added!")
         if mode == "medium":
             with open('questions/medium.txt', 'a') as f:
                 f.write(question + '\n\n')
-            await ctx.channel.send(f"New {mode} question added!")
+            await ctx.send(f"New {mode} question added!")
         if mode == "hard":
             with open('questions/hard.txt', 'a') as f:
                 f.write(question + '\n\n')
-            await ctx.channel.send(f"New {mode} question added!")
+            await ctx.send(f"New {mode} question added!")
 
-    else:
-        await ctx.channel.send(
-            "Please make sure to specify the difficulty of your problem. Easy, medium, or hard"
-        )
+        embed = discord.Embed(
+            title="Thank You!",
+            description=
+            "Thank you for adding a question! It is very beneficial to other's because they are able to look at your question and solve it! This then helps then grow in their coding skills! Have fun learning!",
+            color=discord.Color.blue())
+        await dm.send(embed=embed)
 
 
 @bot.command(name="q")
@@ -52,7 +55,7 @@ async def q(ctx):
                 embed = discord.Embed(title="Easy Problems To Solve",
                                       description=questions,
                                       color=discord.Color.blue())
-            await ctx.channel.send(embed=embed)
+            await ctx.send(embed=embed)
 
     if difficulty == "medium":
         with open(f'questions/{difficulty}.txt', 'r') as f:
@@ -62,7 +65,7 @@ async def q(ctx):
                 embed = discord.Embed(title="Medium Problems To Solve",
                                       description=questions,
                                       color=discord.Color.blue())
-            await ctx.channel.send(embed=embed)
+            await ctx.send(embed=embed)
 
     if difficulty == "hard":
         with open(f'questions/{difficulty}.txt', 'r') as f:
@@ -72,15 +75,22 @@ async def q(ctx):
                 embed = discord.Embed(title="Hard Problems To Solve",
                                       description=questions,
                                       color=discord.Color.blue())
-            await ctx.channel.send(embed=embed)
+            await ctx.send(embed=embed)
 
 
 @bot.command(name="help")
 async def help(ctx):
     embed = discord.Embed(title="Python Bot Commands",
-                          description=commands,
+                          description="",
                           color=discord.Color.blue())
-    await ctx.channel.send(embed=embed)
+
+    embed.set_footer(
+        text=
+        "Remember to specify the difficulty of your problem when using `problem` or `q`"
+    )
+    embed.add_field(name="Commands", value="`problem`, `q`, `help`")
+    embed.add_field(name="Difficulties", value="`easy`, `medium`, or `hard`")
+    await ctx.send(embed=embed)
 
 
 keep_alive()
